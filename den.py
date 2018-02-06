db130bd75a049ffe1f5db20b71af6e56e0a32df2
ziuteg <siudek.lukasz@gmail.com>
2018-02-06 04:07:50 +0100

    loss function and selective training partial implementation

--- den.py
+++ den.py
@@ -1,28 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
+CUDA = True
 
 def main():
     #TODO: get datasets, tau (threshold), sigma (threshold)...
     pass
 
-def incremental_learning(dataset, tau, sigma):
+def setup_model():
+    model = AlexNet(num_classes=100)
+    model = torch.nn.DataParallel(model)
+    if CUDA:
+        model = model.cuda
+    return model
+
+def loss2(model, outputs, targets):
+    loss = nn.CrossEntropyLoss()
+    coeff = 1e-4
+    reg = 0
+    for layer in model.parameters():
+        reg += torch.norm(layer, 1)
+    return loss(outputs, targets) + coeff * reg
+
+def loss3(model, outputs, targets):
+    loss = nn.CrossEntropyLoss()
+    coeff = 1e-4
+    layers = [p for p in model.parameters()]
+    reg = torch.norm(layers[-1], 1)
+    return loss(outputs, targets) + coeff * reg
+
+def loss4(model, outputs, targets):
+    pass
+
+def bfs(model):
+    pass
+
+def incremental_learning(datasets, tau, sigma):
+
+    model = setup_model()
 
     for t, dataset in enumerate(datasets):
         if t == 0:
-            #TODO: train weights W_0 using eq (2)
+            criterion = lambda outputs, targets: 
+                            loss1(model, output, targets)
+            train(dataset, model, loss1)
         else:
-            
-            loss, weights = selective_training(D, W)
+            loss, model = selective_training(prev_model)
+            if loss > tau:
+                model = dynamic_network_expansion(model)
 
-
-def selective_training(dataset, prev_params):
+def selective_training(model):
     """Finds neurons that are relevant to the new
        task and retrains the network parameters
        associated with them.
     """
-    #TODO: implementation
-    pass
+     # freeze all layers except the last one
+     layers = [p for p in model.parameters()]
+     for layer in layers[:-1]:
+         layer.requires_grad = False
 
-def dynamic_network_expansion(dataset, tau):
+    # train the network and receive sparse
+    # connections on the last layer
+    train(dataset, model, loss3)
+
+    # use breadth-first search on the network
+    # to receive set of affected neurons
+    subnetwork = bfs(model)
+
+    # train only the weights of the acquired
+    # subnetwork
+    train(dataset, subnetwork, loss4)
+
+def dynamic_network_expansion(weights, tau):
     """Expands the network capacity in a top-down
        manner, while eliminating any unnecessary
        neurons using group-sparsity regularization.
@@ -30,7 +80,7 @@ def dynamic_network_expansion(dataset, tau):
     #TODO: implementation
     pass
 
-def network_split_duplication(weight, sigma):
+def network_split_duplication(weights, sigma):
     """Calculates the drift for each unit to
        identify units that have drifted too much
        from their original values during training
