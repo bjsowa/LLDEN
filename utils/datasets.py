78fc12f41630c186e6d0e854e06ed8dabd8d1309
Błażej Sowa <bsowa123@gmail.com>
2018-02-15 14:42:50 +0000

    fix loading mnist dataset

--- utils/datasets.py
+++ utils/datasets.py
@@ -24,13 +24,13 @@ def load_MNIST(batch_size = 256, num_workers = 4):
     allset = ConcatDataset([trainset, testset])
     labels = list(i[1] for i in allset)
 
-    trainsampler = ClassSampler(labels, classes, amount=1000)
+    trainsampler = ClassSampler(labels, range(10), amount=1000)
     trainloader = DataLoader(allset, batch_size=batch_size, sampler=trainsampler, num_workers=num_workers)
 
-    validsampler = ClassSampler(labels, classes, amount=200, start_from=1000)
+    validsampler = ClassSampler(labels, range(10), amount=200, start_from=1000)
     validloader = DataLoader(allset, batch_size=batch_size, sampler=validsampler, num_workers=num_workers)
 
-    testsampler = ClassSampler(labels, classes, amount=5000, start_from=1200)
+    testsampler = ClassSampler(labels, range(10), amount=5000, start_from=1200)
     testloader = DataLoader(allset, batch_size=batch_size, sampler=testsampler, num_workers=num_workers)
 
     return (trainloader, validloader, testloader)
